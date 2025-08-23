47ca76c3d12c2050fba9e07a3e3aa7a6c6737439
Anuj Chaudhari <anujpvt2311@gmail.com>
2025-08-23 18:02:56 +0530

    5th com

--- Client/src/utils/axiosInstance.js
+++ Client/src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const axiosInstance = axios.create({
-    baseURL:"https://shortly-6c0v.onrender.com",
+    baseURL:"http://localhost:3000",
     timeout:10000, //10s
     withCredentials:true
 })
